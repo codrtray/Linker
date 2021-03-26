95410ae44ac138b587bc23660376790a1b1af056
Dmitry <dmirtray@gmail.com>
2021-03-26 10:50:42 +0500

    ParameterizedTest Csv

---
+++ common/src/test/java/com/dmi/linker/mapper/TaskMapperTest.java
@@ -0,0 +1,47 @@
+package com.dmi.linker.mapper;
+
+import com.dmi.linker.entity.Task;
+import com.dmi.linker.model.TaskResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+
+class TaskMapperTest {
+
+    private TaskMapper taskMapper;
+
+    @BeforeEach
+    void  init() {
+        taskMapper = new TaskMapperImpl();
+    }
+
+    @DisplayName("test task mapper")
+    @ParameterizedTest
+    @CsvSource({"task1, description1","task2, description2"})
+    void taskMapperTest(ArgumentsAccessor argumentsAccessor) {
+
+        final var task = getTask(argumentsAccessor);
+        final var expectedTaskResponse = getTaskResponse(argumentsAccessor);
+
+        final var actualTaskResponse = taskMapper.taskToTaskResponse(task);
+        assertThat(expectedTaskResponse, samePropertyValuesAs(actualTaskResponse));
+    }
+
+    private Task getTask(ArgumentsAccessor argumentsAccessor) {
+        final var task = new Task();
+        task.setName(argumentsAccessor.getString(0));
+        task.setName(argumentsAccessor.getString(1));
+        return task;
+    }
+    private TaskResponse getTaskResponse(ArgumentsAccessor argumentsAccessor) {
+        final var taskResponse = new TaskResponse();
+        taskResponse.setName(argumentsAccessor.getString(0));
+        taskResponse.setName(argumentsAccessor.getString(1));
+        return taskResponse;
+    }
+}
